becc772569396be698f44789d5f3504bcaf911d8
andriihaidai <haidai.andriy@gmail.com>
2021-04-21 10:21:01 +0300

    method/pop

--- MyArray/script.js
+++ MyArray/script.js
@@ -1,44 +1,45 @@
-function MyArray() {
+function MyArray(...args) {
 
-    for (let i = 0; i < arguments.length; i++) {
-        this[i] = arguments[i];
+    for (let i = 0; i < args.length; i++) {
+        this[i] = args[i];
     }
 
     Object.defineProperty(this, 'length', {
         get: function() {
-            return Object.keys(this).length
-        }
+            return Object.keys(this).length;
+        },
     });
+
 }
 
-MyArray.prototype.push = function() {
-
-    for (let i = 0; i < arguments.length; i++) {
-        this[this.length] = arguments[i];
-        this.length++;
+MyArray.prototype.push = function(...args) {
+    if (args) {
+        for (let i = 0; i < args.length; i++) {
+            this[this.length] = args[i];
+        }
     }
 
-    return this.length;
+    return (this.length);
 }
 
-MyArray.prototype.unshift = function() {
+MyArray.prototype.unshift = function(...args) {
 
 
-    for (let j = arguments.length; j > 0; j--) {
+    for (let j = args.length; j > 0; j--) {
         for (let i = this.length; i > 0; i--) {
             this[i] = this[i - 1];
         }
     }
 
-    for (let i = 0; i < arguments.length; i++) {
-        this[i] = arguments[i];
+    for (let i = 0; i < args.length; i++) {
+        this[i] = args[i];
     }
 
     return this.length;
 };
 
 MyArray.prototype.map = function(callback) {
-    let res = [];
+    let res = new MyArray();
 
     for (let i = 0; i < this.length; i++) {
         res[i] = callback(this[i], i, this);
@@ -55,5 +56,33 @@ MyArray.prototype.forEach = function(callback) {
     return undefined;
 }
 
+MyArray.prototype.filter = function(callback) {
+    let res = new MyArray();
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
+            res[res.length] = this[i];
+        }
+    }
+
+    return res;
+}
+
+MyArray.prototype.pop = function() {
+    const deletedItem = this[this.length - 1];
+
+    // if (this.length == 0) {
+    //     return undefined;
+    // }
+
+    delete this[this.length - 1];
+
+
+    return deletedItem;
+};
+
 
 let arr = new MyArray();
+console.log(arr);
+console.log(arr.pop());
+arr.pop();
+console.log(arr);
