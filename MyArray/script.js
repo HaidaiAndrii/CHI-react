25cc57e8b621e49b9bb67aa8ec93efdd09754860
andriihaidai <haidai.andriy@gmail.com>
2021-04-23 10:05:56 +0300

    method/sort

--- MyArray/script.js
+++ MyArray/script.js
@@ -109,37 +109,52 @@ MyArray.prototype.reduce = function(callback, initialValue) {
     return currentValue;
 };
 
-MyArray.prototype.sortFunction = (fElement, sElement) => {
-    let firstElement = toString(fElement);
-    let secondElement = toString(sElement);
-
-    if (firstElement > secondElement) {
-        return 1;
+MyArray.prototype.sort = function(callback) {
+    if (typeof callback !== 'function') {
+        throw new TypeError(callback + ' is not a function');
     }
 
-    if (firstElement < secondElement) {
-        return -1;
-    }
-
-    return 0;
-}
-
-MyArray.prototype.sort = function(callback = this.sortFunction) {
-    for (let i = 1; i < this.length; i++) {
-
-        for (let j = i; j > 0; j--) {
-
-            if (callback(this[j], this[j - 1])) {
-                const temp = this[j];
-                this[j] = this[j - 1];
-                this[j - 1] = temp;
-            } else {
-                break;
+    if (callback) {
+        for (let i = 0; i < this.length - 1; i++) {
+            for (let j = 0; j < this.length - 1; j++) {
+                if (callback(this[j], this[j + 1]) > 0) {
+                    let temp = this[j];
+                    this[j] = this[j + 1];
+                    this[j + 1] = temp;
+                }
             }
         }
+    } else {
+        for (let i = 1; i < this.length; i++) {
+            const current = this[i];
+            let j = i;
+            while (j > 0 && this[j - 1] > current) {
+                this[j] = this[j - 1];
+                j--;
+            }
+            this[j] = current;
+        }
     }
-
     return this;
-}
+};
+
+MyArray.prototype.from = function(array, callback, thisArg) {
+    if (array === undefined) {
+        return;
+    }
+    const newArray = new MyArray();
+    let i = 0;
+    if (typeof array[Symbol.iterator]) {
+        for (let value of array) {
+            if (callback) {
+                newArray.push(callback.call(thisArg, value, i, array));
+            } else {
+                newArray.push(value);
+            }
+            i++;
+        }
+    }
+    return newArray;
+};
 
 let arr = new MyArray(1, 2, 3, 4, 5, 'abc,', `asdas`, { 1: 'asda' });
