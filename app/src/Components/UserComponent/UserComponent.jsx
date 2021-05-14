2e06b53e12e0392e060083ebb5d32c58d1372050
andriihaidai <haidai.andriy@gmail.com>
2021-05-14 15:31:42 +0300

    user component was improoved

--- app/src/Components/UserComponent/UserComponent.jsx
+++ app/src/Components/UserComponent/UserComponent.jsx
@@ -5,26 +5,28 @@ import {
 } from "react-router-dom";
 import styles from './styles.module.css';
 
-
-
 export function UserComponent({ isLogined }) {
     let location  = useLocation();
-    let [id, setId] = useState('');
-    let [name, setName] = useState('');
-    let [userName, setUserName] = useState('');
-    let [email, setEmail] = useState('');
-    let [website, setWebsite] = useState('');
     let history = useHistory() || [];
+
+    // let [id, setId] = useState('');
+    // let [name, setName] = useState('');
+    // let [userName, setUserName] = useState('');
+    // let [email, setEmail] = useState('');
+    // let [website, setWebsite] = useState('');
+
+    let [user , setUser] = useState({});
     
     function getUser() {
         fetch(`https://jsonplaceholder.typicode.com/users/${location.pathname.slice(7)}`)
             .then(response => response.json())
             .then(json => {
-                setName(json.name);
-                setId(json.id);
-                setUserName(json.username);
-                setEmail(json.email);
-                setWebsite(json.website)
+                // setName(json.name);
+                // setId(json.id);
+                // setUserName(json.username);
+                // setEmail(json.email);
+                // setWebsite(json.website)
+                setUser(json);
             });
     }
 
@@ -33,14 +35,6 @@ export function UserComponent({ isLogined }) {
     },[]);
 
     async function saveChanges() {
-        let user = {
-            id,
-            name,
-            username: userName,
-            email,
-            website
-        }
-
          await fetch('https://jsonplaceholder.typicode.com/posts', {
             method: 'POST',
             body: JSON.stringify(user),
@@ -52,29 +46,61 @@ export function UserComponent({ isLogined }) {
           history.push('/table');
     }
 
+    function changeUserInfo(value, field) {
+        let us = {
+            ...user,
+        }
 
+        us[field] = value
+        console.log(value,field, us)
+        setUser(us)
+        // console.log(info.value, info.id)
+    }
+
+    const FIELDS = ['id', 'name', 'username', 'email', 'website'];
+    
     return(
         <div className={styles.table}>
             <h2>User</h2>
             <div className={styles.inputsSection}>
-                <div className={styles.tableSection}>
-                    <label className={styles.span} for='id'>Id</label><input className={styles.input} id='id' type="text" value={id} onChange={(e) => setId(e.target.value)} name="id" readOnly={!isLogined} />
-                </div>
-                <div className={styles.tableSection}>
-                    <label for='name' className={styles.span}>Name</label><input id='name' className={styles.input} type="text" value={name} onChange={(e) => setName(e.target.value)} name="name" readOnly={!isLogined} />
+            
+            { FIELDS.map(field => {
+                    return (
+                        <div className={styles.tableSection}>
+                            <label className={styles.span} for={field}>{field}</label>
+                            <input className={styles.input} id={field} type="text" value={user[field]} onChange={e => changeUserInfo(e.target.value, e.target.id)} readOnly={!isLogined} />
+                        </div>
+                    ) 
+            
+            })
+            }
+    
+
+                {/* <div className={styles.tableSection}>
+                    <label className={styles.span} for='id'>Id</label>
+                    <input className={styles.input} id='id' type="text" value={id} onChange={(e) => setId(e.target.value)} name="id" readOnly={!isLogined} />
                 </div>
 
                 <div className={styles.tableSection}>
-                    <label for='username' className={styles.span}>Username</label><input id='username' className={styles.input} type="text" value={userName} onChange={(e) => setUserName(e.target.value)} name="userName" readOnly={!isLogined} />
+                    <label for='name' className={styles.span}>Name</label>
+                    <input id='name' className={styles.input} type="text" value={name} onChange={(e) => setName(e.target.value)} name="name" readOnly={!isLogined} />
                 </div>
 
                 <div className={styles.tableSection}>
-                    <label for='email' className={styles.span}>Email</label><input id='email' className={styles.input} type="text" value={email} onChange={(e) => setEmail(e.target.value)} name="email" readOnly={!isLogined} />
+                    <label for='username' className={styles.span}>Username</label>
+                    <input id='username' className={styles.input} type="text" value={userName} onChange={(e) => setUserName(e.target.value)} name="userName" readOnly={!isLogined} />
                 </div>
 
                 <div className={styles.tableSection}>
-                    <label for='website' className={styles.span}>Website</label><input id='website' className={styles.input} type="text" value={website} onChange={(e) => setWebsite(e.target.value)} name="website" readOnly={!isLogined} />
+                    <label for='email' className={styles.span}>Email</label
+                    ><input id='email' className={styles.input} type="text" value={email} onChange={(e) => setEmail(e.target.value)} name="email" readOnly={!isLogined} />
                 </div>
+
+                <div className={styles.tableSection}>
+                    <label for='website' className={styles.span}>Website</label>
+                    <input id='website' className={styles.input} type="text" value={website} onChange={(e) => setWebsite(e.target.value)} name="website" readOnly={!isLogined} />
+                </div> */}
+
             </div>
 
             <button className={styles.button} onClick={saveChanges}>Save changes</button>
